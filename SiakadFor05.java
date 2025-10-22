0bff7ed13d18155d4b096b6d92d3141f9b2f19fc
azzahraliarahman <azzahraliarahma@gmail.com>
2025-10-22 19:20:33 +0700

    Modifikasi 1

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -1,23 +1,44 @@
 import java.util.Scanner;
+
 public class SiakadFor05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double  nilai, tetinggi = 0, terendah = 100;
-        
+
+        double nilai;
+        double tertinggi = 0;
+        double terendah = 100;
+
+        int jmlLulus = 0;
+        int jmlTdkLulus = 0;
+        int btsNilai = 60;
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
-            
-            if (nilai > tetinggi) {
-                tetinggi = nilai;
+
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
-
-            System.out.println("Nilai tertinggi saat ini: " + tetinggi);
-            System.out.println("Nilai terendah saat ini: " + terendah);
+            
+    
+            if (nilai >= btsNilai) {
+                jmlLulus++;
+            } else {
+                jmlTdkLulus++;
+            }
         }
+    
+        sc.close();
+
+        System.out.println("------------------------------------");
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        
+        
+        System.out.println("Jumlah mahasiswa lulus: " + jmlLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jmlTdkLulus);
     }
 }
-        
